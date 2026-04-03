46347b5a40d516963b0a57b4adf5b55d422ef5fc
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2026-04-03 13:30:04 -0300

    refactor(db): config db

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,25 +4,16 @@ import fs from 'fs'
 const DEFAULT_SERVER_PORT = 3000;
 
 
-const requiredRegisterSecretSchema = z.preprocess(
-  (value) => (typeof value === "string" ? value.trim() : ""),
-  z.string().min(1, "Missing required environment variable: REGISTER_SECRET")
-);
-
-const dbPathSchema = z.string().refine(
-  (path) => fs.existsSync(path), 
-  { message: "Database file does not exist" }
-);
-
 const serverConfigSchema = z.object({
-  DATABASE_URL: dbPathSchema,
+  DATABASE_URL: z.string(),
   SERVER_PORT: z.coerce.number().int().positive().default(DEFAULT_SERVER_PORT),
-  REGISTER_SECRET: requiredRegisterSecretSchema,
+  //TODO: passar para uso de enum
   STORAGE_PROVIDER: z.string().trim(),
-  STORAGE_HOST: z.string().trim().default(""),
-  STORAGE_USER: z.string().trim().default(""),
-  STORAGE_PASSWORD: z.string().default(""),
-  STORAGE_BASE_PATH: z.string().trim(),
+  //TODO: remover default e tornar obrigatorio
+  STORAGE_HOST: z.string().trim().default(''),
+  STORAGE_USER: z.string().trim().default(''),
+  STORAGE_PASSWORD: z.string().default(''),
+  STORAGE_BASE_PATH: z.string().trim().default(''),
 });
 
 export const env = serverConfigSchema.parse(process.env);
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,10 @@
 import { env } from '@/config/env';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { schema } from './schemas/index';
+import { Database } from "bun:sqlite";
 
-export const db = drizzle(env.DATABASE_URL, {
+const database = new Database(env.DATABASE_URL, {create: true});
+export const db = drizzle(database, {
     schema,
 	casing: 'snake_case',
 });
