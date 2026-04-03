9b3705d1401d8cfe8ac87c6af00711a7fcb2dfa7
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2026-04-03 15:31:36 -0300

    refactor(release): removed unused errors

--- src/db/errors/release.errors.ts
+++ src/db/errors/release.errors.ts
@@ -19,27 +19,3 @@ export class InvalidReleaseVersionError extends ReleaseDomainError {
     this.version = version;
   }
 }
-
-export class InvalidReleaseIdentifierError extends ReleaseDomainError {
-  constructor() {
-    super(
-      "Failed to create release: invalid release identifier.",
-      "INVALID_RELEASE_IDENTIFIER"
-    );
-  }
-}
-
-export class ReleaseLookupError extends ReleaseDomainError {
-  constructor(message: string = "Failed to load release from database.") {
-    super(message, "RELEASE_LOOKUP_FAILED");
-  }
-}
-
-export class DuplicateReleaseVersionError extends ReleaseDomainError {
-  readonly version: string;
-
-  constructor(version: string) {
-    super(`Release version "${version}" already exists.`, "DUPLICATE_RELEASE_VERSION");
-    this.version = version;
-  }
-}
