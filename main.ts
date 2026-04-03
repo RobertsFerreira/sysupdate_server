219cb6822405e232ed26fadb8907cad783dc6c26
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2026-04-03 11:11:42 -0300

    refactor(db): configure db

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
 import { env } from "@/config/env";
-import { initializeDatabase } from "@/db/schemas";
 import { routes } from "@/routes/routes";
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-  .onStart(() => {
-    initializeDatabase();
-  })
   .use(routes)
   .listen(env.SERVER_PORT);
 
